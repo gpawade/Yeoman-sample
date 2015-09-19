ac9330a060c420818d2042722c07bfbec2dbfefa
gpawade <gap.subscribe@gmail.com>
2015-09-19 16:58:14 +0530

    yo generation for copy jquery & bootstrap

--- generator-sample/app/index.js
+++ generator-sample/app/index.js
@@ -8,14 +8,101 @@ module.exports = generators.Base.extend({
 
 	 	//Next, add your custom code 
 	 	console.log('constructor running...');
+
+	 	//this make 'appname' a required argument
+	 	this.argument('appname', { type: String, required : true});
+
+	 	// and access it.
+	 	this.appname = this.appname;
+
+
+	 	// options are commmand line flag... e.g. --jquery
+	 	this.option('jquery', { 
+	 		desc : 'include jquery lib in application', 
+	 		type : Boolean
+	 	});
+	 	this.option('bootstrap', { 
+	 		desc : 'include bootstrap lib in application',
+	 		type : Boolean
+	 	});
+
+	 	//console.log('options', this.options.jquery);
+
 	 },
 
-	 method1 : function(){
-	 	console.log('method1 is running..');
+	 copyTheme : function(){
+
+	 	if(this.options.bootstrap){
+	 		this.log('coping theme....');
+
+	 		this.fs.copy(
+	 				this.templatePath('theme'),
+	 				this.destinationPath('theme')
+	 			);
+	 	}
 	 },
 
-	 method2 : function(){
+	 copyJS : function(){
 
-	 	console.log('method2 is running...');
+	 	this.log('coping js....');
+		if(this.options.jquery){
+	 		this.fs.copy(
+	 				this.templatePath('js/jquery.min.js'),
+	 				this.destinationPath('js/jquery.min.js')
+	 			);
+	 	}
+
+	 	if(this.options.bootstrap){
+	 		this.fs.copy(
+	 				this.templatePath('js/bootstrap.min.js'),
+	 				this.destinationPath('js/bootstrap.min.js')
+	 			);
+	 	}
+
+	 	
+
+	 },
+
+	 render : function(){
+	 	var _this = this;
+	 	var templateObj = {
+	 		jquery : _this.options.jquery,
+	 		bootstrap : _this.options.bootstrap
+	 	};
+
+	 	this.fs.copyTpl(
+	      this.templatePath('index.html'),
+	      this.destinationPath('index.html'),
+	      templateObj
+	    );
 	 }
+
+	 /*
+	 jquerySetup : function(){
+	 	console.log('jquerySetup is running..');
+
+	 	if(this.options.jquery)
+	 		this.log('jquery is enable');
+
+	 	
+	 },
+
+	 bootstrapSetup : function(){
+
+	 	this.log('bootstrapSetup is running...');
+
+	 	if(this.options.bootstrap)
+	 		this.log('bootstrap is enable');
+	 },
+
+
+	 path : function(){
+	 	//Destination path 
+	 	this.log(this.destinationRoot());
+	 	this.log(this.destinationPath('index.html'));
+
+	 	//Source Path
+	 	this.log(this.sourceRoot());
+	 	this.log(this.templatePath('index.js'));
+	 }*/
 });
